242439ea1c3d2f1f1ed995a3da8896696ff1b2d2
David Palacios <davidpa7710@gmail.com>
2022-03-22 20:25:23 -0600

    add: basic componet in react-vite with functions

---
+++ src/Components/App/index.jsx
@@ -0,0 +1,9 @@
+import './app.scss'
+
+const App = () => {
+    return(
+      <p className='text'>Componente Funcional</p>
+  )
+}
+
+export { App }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import './Styles/index.css'
+import { App } from './Components/App'
 
 render(
   <React.StrictMode>
     <p>Vite react App</p>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
