87b4cc7c6a3585919cad917f7741c2680a7f864c
David Palacios <davidpa7710@gmail.com>
2022-03-22 19:44:46 -0600

    init: first commit with vite-react app

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+import './Styles/index.css'
+
+render(
+  <React.StrictMode>
+    <p>Vite react App</p>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
